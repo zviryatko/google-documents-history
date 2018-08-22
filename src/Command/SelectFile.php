e0f481ef7663369e5b179d9b75291d2a81c711cd
Alex Davyskiba <sanya.davyskiba@gmail.com>
2018-08-22 23:10:48 +0300

    Fix code styles to PSR1/2

--- src/Command/SelectFile.php
+++ src/Command/SelectFile.php
@@ -14,52 +14,58 @@ use Symfony\Component\Console\Question\ChoiceQuestion;
 use function Functional\map as map;
 use function Functional\reindex as reindex;
 
-final class SelectFile extends Command {
-  /**
-   * @var \Google_Service_Drive
-   */
-  private $service;
+final class SelectFile extends Command
+{
 
-  public function __construct(string $name, \Google_Service_Drive $service) {
-    parent::__construct($name);
-    $this->service = $service;
-  }
+    /**
+     * @var \Google_Service_Drive
+     */
+    private $service;
 
-  private function options() {
-    $optParams = array(
-      'pageSize' => 10,
-      'fields' => 'nextPageToken, files(id, name)',
-    );
-    $results = $this->service->files->listFiles($optParams);
-
-    $getId = function (\Google_Service_Drive_DriveFile $file) {
-      return $file->getId();
-    };
-    $getName = function (\Google_Service_Drive_DriveFile $file) {
-      return $file->getName();
-    };
-    return map(reindex($results->getFiles(), $getId), $getName);
-  }
-
-  public function execute(InputInterface $input, OutputInterface $output) {
-    $helper = $this->getHelper('question');
-    $question = new ChoiceQuestion(
-      'Please select the file',
-      $this->options()
-    );
-    $question->setErrorMessage('File %s is invalid.');
-
-    $fileId = $helper->ask($input, $output, $question);
-    try {
-      $this->export($fileId);
-    } catch (\Google_Service_Exception $e) {
-      $data = json_decode($e->getMessage());
-      throw new RuntimeException($data->error->message, $data->error->code, $e);
+    public function __construct(string $name, \Google_Service_Drive $service)
+    {
+        parent::__construct($name);
+        $this->service = $service;
     }
-  }
 
-  private function export($fileId) {
-    $file = $this->service->files->get($fileId);
-    $export = $this->service->files->export($fileId, 'application/pdf', ['alt' => 'media']);
-  }
+    private function options()
+    {
+        $optParams = [
+            'pageSize' => 10,
+            'fields' => 'nextPageToken, files(id, name)',
+        ];
+        $results = $this->service->files->listFiles($optParams);
+
+        $getId = function (\Google_Service_Drive_DriveFile $file) {
+            return $file->getId();
+        };
+        $getName = function (\Google_Service_Drive_DriveFile $file) {
+            return $file->getName();
+        };
+        return map(reindex($results->getFiles(), $getId), $getName);
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $helper = $this->getHelper('question');
+        $question = new ChoiceQuestion(
+            'Please select the file',
+            $this->options()
+        );
+        $question->setErrorMessage('File %s is invalid.');
+
+        $fileId = $helper->ask($input, $output, $question);
+        try {
+            $this->export($fileId);
+        } catch (\Google_Service_Exception $e) {
+            $data = json_decode($e->getMessage());
+            throw new RuntimeException($data->error->message, $data->error->code, $e);
+        }
+    }
+
+    private function export($fileId)
+    {
+        $file = $this->service->files->get($fileId);
+        $export = $this->service->files->export($fileId, 'application/pdf', ['alt' => 'media']);
+    }
 }
--- src/GoogleServiceProvider.php
+++ src/GoogleServiceProvider.php
@@ -8,60 +8,64 @@ namespace zviryatko\GDocHist;
 
 use XdgBaseDir\Xdg;
 
-final class GoogleServiceProvider {
-  /**
-   * Returns an authorized API client.
-   * @return \Google_Client
-   *    The authorized client object
-   * @throws \Google_Exception
-   */
-  private function getClient() {
-    $client = new \Google_Client();
-    $client->setApplicationName('Google Doc History');
-    $client->setScopes([
-      \Google_Service_Drive::DRIVE_METADATA,
-      \Google_Service_Drive::DRIVE_FILE,
-    ]);
-    $client->setAuthConfig('credentials.json');
-    $client->setAccessType('offline');
+final class GoogleServiceProvider
+{
 
-    // Load previously authorized credentials from a file.
-    $xdg = new Xdg();
-    $cacheDir = $xdg->getHomeCacheDir();
-    $credentialsPath = $cacheDir . '/token.json';
-    if (file_exists($credentialsPath)) {
-      $accessToken = json_decode(file_get_contents($credentialsPath), TRUE);
+    /**
+     * Returns an authorized API client.
+     *
+     * @return \Google_Client
+     *    The authorized client object
+     * @throws \Google_Exception
+     */
+    private function getClient()
+    {
+        $client = new \Google_Client();
+        $client->setApplicationName('Google Doc History');
+        $client->setScopes([
+            \Google_Service_Drive::DRIVE_METADATA,
+            \Google_Service_Drive::DRIVE_FILE,
+        ]);
+        $client->setAuthConfig('credentials.json');
+        $client->setAccessType('offline');
+
+        // Load previously authorized credentials from a file.
+        $xdg = new Xdg();
+        $cacheDir = $xdg->getHomeCacheDir();
+        $credentialsPath = $cacheDir . '/token.json';
+        if (file_exists($credentialsPath)) {
+            $accessToken = json_decode(file_get_contents($credentialsPath), true);
+        } else {
+            // Request authorization from the user.
+            $authUrl = $client->createAuthUrl();
+            printf("Open the following link in your browser:\n%s\n", $authUrl);
+            print 'Enter verification code: ';
+            $authCode = trim(fgets(STDIN));
+
+            // Exchange authorization code for an access token.
+            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+
+            // Store the credentials to disk.
+            if (!file_exists(dirname($credentialsPath))) {
+                mkdir(dirname($credentialsPath), 0700, true);
+            }
+            file_put_contents($credentialsPath, json_encode($accessToken));
+            printf("Credentials saved to %s\n", $credentialsPath);
+        }
+        $client->setAccessToken($accessToken);
+
+        // Refresh the token if it's expired.
+        if ($client->isAccessTokenExpired()) {
+            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
+        }
+        return $client;
     }
-    else {
-      // Request authorization from the user.
-      $authUrl = $client->createAuthUrl();
-      printf("Open the following link in your browser:\n%s\n", $authUrl);
-      print 'Enter verification code: ';
-      $authCode = trim(fgets(STDIN));
 
-      // Exchange authorization code for an access token.
-      $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-
-      // Store the credentials to disk.
-      if (!file_exists(dirname($credentialsPath))) {
-        mkdir(dirname($credentialsPath), 0700, TRUE);
-      }
-      file_put_contents($credentialsPath, json_encode($accessToken));
-      printf("Credentials saved to %s\n", $credentialsPath);
+    public function create(): \Google_Service_Drive
+    {
+        // Get the API client and construct the service object.
+        $client = $this->getClient();
+        return new \Google_Service_Drive($client);
     }
-    $client->setAccessToken($accessToken);
-
-    // Refresh the token if it's expired.
-    if ($client->isAccessTokenExpired()) {
-      $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-      file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
-    }
-    return $client;
-  }
-
-  public function create(): \Google_Service_Drive {
-    // Get the API client and construct the service object.
-    $client = $this->getClient();
-    return new \Google_Service_Drive($client);
-  }
 }
