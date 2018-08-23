bdf62801031ed94d2c3834664005d90ab12b4991
Alex Davyskiba <sanya.davyskiba@gmail.com>
2018-08-23 08:59:47 +0300

    Add file creating and download

--- src/Command/SelectFile.php
+++ src/Command/SelectFile.php
@@ -6,6 +6,8 @@
 
 namespace zviryatko\GDocHist\Command;
 
+use GuzzleHttp\Psr7\Response;
+use League\HTMLToMarkdown\HtmlConverter;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\Console\Input\InputInterface;
@@ -13,6 +15,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
 use function Functional\map as map;
 use function Functional\reindex as reindex;
+use Symfony\Component\Console\Question\Question;
 
 final class SelectFile extends Command
 {
@@ -31,7 +34,7 @@ final class SelectFile extends Command
     private function options()
     {
         $optParams = [
-            'pageSize' => 10,
+            //'pageSize' => 10,
             'fields' => 'nextPageToken, files(id, name)',
         ];
         $results = $this->service->files->listFiles($optParams);
@@ -50,12 +53,16 @@ final class SelectFile extends Command
         $helper = $this->getHelper('question');
         $question = new ChoiceQuestion(
             'Please select the file',
-            $this->options()
+            $this->options() + ['' => 'Create new file'],
+            ''
         );
         $question->setErrorMessage('File %s is invalid.');
 
         $fileId = $helper->ask($input, $output, $question);
         try {
+            if (empty($fileId)) {
+                $fileId = $this->create($input, $output);
+            }
             $this->export($fileId);
         } catch (\Google_Service_Exception $e) {
             $data = json_decode($e->getMessage());
@@ -66,6 +73,63 @@ final class SelectFile extends Command
     private function export($fileId)
     {
         $file = $this->service->files->get($fileId);
-        $export = $this->service->files->export($fileId, 'application/pdf', ['alt' => 'media']);
+        $export = $this->service->files->export($fileId, 'application/pdf', [
+            'alt' => 'media',
+            'mimeType' => 'text/html',
+//            'mimeType' => 'application/pdf',
+        ]);
+        if ($export instanceof Response) {
+            $path = 'data/' . $file->getName() . '.html';
+//            $path = 'data/' . $file->getName() . '.pdf';
+            $this->prepareFile($path);
+            $content = (string) $export->getBody();
+            $content = $this->purify($content);
+//            $content = $this->convertToMd($content);
+            $this->saveFile($path, $content);
+        }
+    }
+
+    private function create(InputInterface $input, OutputInterface $output): string
+    {
+        /** @var \Symfony\Component\Console\Helper\QuestionHelper $helper */
+        $helper = $this->getHelper('question');
+        $name = $helper->ask($input, $output, new Question('Provide file name: '));
+
+        $fileMetadata = new \Google_Service_Drive_DriveFile(array(
+            'name' => $name,
+            'mimeType' => 'application/vnd.google-apps.document'));
+        $file = $this->service->files->create($fileMetadata, array(
+            'fields' => 'id'));
+        return $file->id;
+    }
+
+    private function prepareFile(string $path): void
+    {
+        if (!file_exists(dirname($path))) {
+            mkdir(dirname($path));
+        }
+        if (!file_exists($path)) {
+            touch($path);
+        }
+    }
+
+    private function convertToMd(string $content): string
+    {
+        $converter = new HtmlConverter(array('strip_tags' => true));
+        return $converter->convert($content);
+    }
+
+    private function saveFile(string $path, string $content): void
+    {
+        $out = fopen($path, 'rw+');
+        fwrite($out, $content);
+        fclose($out);
+    }
+
+    private function purify(string $content): string
+    {
+        $config = \HTMLPurifier_Config::createDefault();
+        $purifier = new \HTMLPurifier($config);
+        return $purifier->purify($content);
     }
 }
